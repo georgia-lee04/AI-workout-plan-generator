7a322784c4491350fb09ba6d9efb174e151cb346
georgia-lee04 <xgeorgialee@gmail.com>
2025-02-12 16:38:56 +0000

    Added basic form handling and added Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateWorkout(event) {
+  event.preventDefault();
 
+  new Typewriter("#generator-output", {
+    strings: ["Hello"],
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let workoutPlanForm = document.querySelector("#workout-plan-form");
+workoutPlanForm.addEventListener("submit", generateWorkout);
