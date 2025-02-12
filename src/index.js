d3dfe55031ddf3c8ade3debe4893657ac8a55651
georgia-lee04 <xgeorgialee@gmail.com>
2025-02-12 19:39:32 +0000

    Added AI API integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-function generateWorkout(event) {
-  event.preventDefault();
-
+function displayWorkout(response) {
+  console.log("workout generated");
   new Typewriter("#generator-output", {
-    strings: ["Hello"],
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generateWorkout(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#instructions");
+  let apiKey = "4f9f36e261a3705eo07b4a98ct5b3f15";
+  let prompt = `Generate a workout plan for the gym based on ${instructionsInput.value}`;
+  let context =
+    "You are a knowledgable fitness expert and provide short effective 45 minute gym workouts in ten lines maximum. Your mission is to provide a short 45 minute workout plan that targets the area of the body entered. Make sure to follow the user instructions.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("generating a workout for the gym");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiUrl).then(displayWorkout);
 }
 
 let workoutPlanForm = document.querySelector("#workout-plan-form");
